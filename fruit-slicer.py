70d42f63a287af2fee95d6e95a471db9007992a5
Arnaud Sénégas <arnaud.senegas@laplateforme.io>
2025-01-29 13:37:18 +0100

    feat : add life and game over screen

--- fruit-slicer.py
+++ fruit-slicer.py
@@ -16,6 +16,7 @@ apple = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-
 ice = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/ice.png")
 bomb = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/bomb.png")
 strawberry = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/strawberry.png")
+boom = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/boom.png")
 lives = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/redcross.png")
 
 fruits = [banana, orange, apple, ice, bomb, strawberry]
@@ -26,8 +27,22 @@ GRAVITY  = 2
 alphabet = ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "w", "x", "c", "v", "b", "n"]
 letter = random.choice(alphabet)
 
-def text(text,font, text_color, x, y):
-    screen.blit(text, (x,y))
+def write(texte, text_font, couleur, x, y):
+    texte_surface = text_font.render(texte, True, couleur)
+    screen.blit(texte_surface, (x, y))
+
+def game_over_screen():
+    screen.blit(background, (0, 0)) 
+    screen.blit(boom, (250,0))
+    write(f"GAME OVER !", text_font, BLACK, 600, 375)
+    write(f"1. Play again?", text_font, BLACK, 200, 50)
+    write(f"2. Back to menu?", text_font, BLACK, 950,50 )
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                main_game()
+            if event.key == pygame.K_2:
+                menu()
 
 
 class Objet:
@@ -35,7 +50,7 @@ class Objet:
         self.x = random.randint(150,1000)
         self.y = 700
         self.vitesse_x = random.randint(-15,15)
-        self.vitesse_y = random.randint(-50, -35) 
+        self.vitesse_y = random.randint(-50, -40) 
         self.image = random.choice(fruits)
     
     def update(self):
@@ -48,42 +63,49 @@ class Objet:
 
 def spawn_new_fruits():
     objets.append(Objet())
-    text(letter, text_font, BLACK, (objet_x, objet_y))
+
 
 objets = [Objet()]
 
 def main_game():
-    
+    game_over = False
+    life = 3
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         
-        screen.blit(background, (0, 0)) 
-        screen.blit(lives,(1050,20))
-        screen.blit(lives,(1110,20))
-        screen.blit(lives,(1170,20))
+        screen.blit(background, (0, 0))
+
+        if game_over == True :
+            game_over_screen()
+
+        for i in range(life):
+            screen.blit(lives, (1050 + i * 60, 20))
 
         for objet in objets[:]:
             objet.update()
             objet.display(screen)
 
-        if objet.y > 750 or objet.y<0 or objet.x<0 or objet.x>1250:
+        for objet in objets[:]:
+            if objet.y > 750 or objet.y < 0 or objet.x < 0 or objet.x > 1250:
                 objets.remove(objet)
-                if random.random() < 0.25:
-                    spawn_new_fruits()  
-                    spawn_new_fruits() 
-                    spawn_new_fruits() 
-                if random.random()< 0.05:
-                    spawn_new_fruits() 
-                    spawn_new_fruits() 
-                    spawn_new_fruits() 
-                    spawn_new_fruits() 
+                if random.random() < 0.5 and len(objets) >= 2 :
+                    continue  
                 else:
-                    spawn_new_fruits() 
-                    spawn_new_fruits() 
-                         
+                    if random.random() < 0.5:
+                        spawn_new_fruits()
+                    else:
+                        spawn_new_fruits()
+                        spawn_new_fruits()
+                if objets:
+                    life -= 1
+                    if life == 0:
+                        objets.clear()
+                        game_over = True
+
+
         pygame.display.update()
         clock.tick(FPS)
 
