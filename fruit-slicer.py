4edf8c482848a1b5656321c1203d0021ababf7e9
Arnaud Sénégas <arnaud.senegas@laplateforme.io>
2025-01-30 11:33:50 +0100

    feat : add lifes function and game over screen

--- fruit-slicer.py
+++ fruit-slicer.py
@@ -7,9 +7,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((1250, 750))
 background = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/back.png")
-text_font = pygame.font.SysFont("Arial", 15)
+text_font = pygame.font.SysFont("Arial", 40)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
+ninja = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/ninja.png")
 banana = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/banana.png")
 orange = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/orange.png")
 apple = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-Slicer/images/apple.png")
@@ -21,28 +23,36 @@ lives = pygame.image.load(r"C:/Users/arnau/Documents/La Plateforme/Python/Fruit-
 
 fruits = [banana, orange, apple, ice, bomb, strawberry]
 clock = pygame.time.Clock()
-FPS = 20
+FPS = 30
 GRAVITY  = 2
 
 alphabet = ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "w", "x", "c", "v", "b", "n"]
 letter = random.choice(alphabet)
 
-def write(texte, text_font, couleur, x, y):
+def text(texte, text_font, couleur, x, y):
     texte_surface = text_font.render(texte, True, couleur)
     screen.blit(texte_surface, (x, y))
 
+def menu():
+    screen.blit(background,(0,0))
+    screen.blit(ninja,(0,200))
+    text("Welcome to", text_font, (WHITE), 425, 50)
+    text("NINJA FRUITS", text_font, (WHITE), 300, 100)
+    text("1. PLAY GAME", text_font, (WHITE), 420, 250)
+    text("2. CHANGE YOUR NAME", text_font, (WHITE), 420, 300)
+    text("3. SCORING TABLE", text_font, (WHITE), 420, 350)
+    text("4. EXIT THE GAME (are you sure ?)", text_font, (WHITE), 420, 400)
+    text("ENTER YOUR CHOICE", text_font, (WHITE), 460, 450)
+
 def game_over_screen():
     screen.blit(background, (0, 0)) 
-    screen.blit(boom, (250,0))
-    write(f"GAME OVER !", text_font, BLACK, 600, 375)
-    write(f"1. Play again?", text_font, BLACK, 200, 50)
-    write(f"2. Back to menu?", text_font, BLACK, 950,50 )
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                main_game()
-            if event.key == pygame.K_2:
-                menu()
+    screen.blit(boom, (250,-10))
+    screen.blit(lives, (1050, 20))
+    screen.blit(lives, (1110, 20))
+    screen.blit(lives, (1170, 20))
+    text(f"GAME OVER !", text_font, WHITE, 535, 375)
+    text(f"1. Play again?", text_font, WHITE, 75, 300)
+    text(f"2. Back to menu?", text_font, WHITE, 75,500 )
 
 
 class Objet:
@@ -58,6 +68,7 @@ class Objet:
         self.x += self.vitesse_x
         self.y += self.vitesse_y
 
+
     def display(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
@@ -67,8 +78,16 @@ def spawn_new_fruits():
 
 objets = [Objet()]
 
+game_state = "menu"
+main_loop = True
+
+def reset_game():
+    global objets, life
+    objets = [Objet()] 
+    life = 3 
+
 def main_game():
-    game_over = False
+    reset_game()
     life = 3
     run = True
     while run:
@@ -78,9 +97,6 @@ def main_game():
         
         screen.blit(background, (0, 0))
 
-        if game_over == True :
-            game_over_screen()
-
         for i in range(life):
             screen.blit(lives, (1050 + i * 60, 20))
 
@@ -89,7 +105,7 @@ def main_game():
             objet.display(screen)
 
         for objet in objets[:]:
-            if objet.y > 750 or objet.y < 0 or objet.x < 0 or objet.x > 1250:
+            if objet.y > 745 or objet.y < 5 or objet.x < 5 or objet.x > 1245:
                 objets.remove(objet)
                 if random.random() < 0.5 and len(objets) >= 2 :
                     continue  
@@ -99,15 +115,45 @@ def main_game():
                     else:
                         spawn_new_fruits()
                         spawn_new_fruits()
+
+                if objet.image == bomb or objet.image == ice:
+                    continue 
+
                 if objets:
                     life -= 1
                     if life == 0:
-                        objets.clear()
-                        game_over = True
+                        return "game_over"
 
 
         pygame.display.update()
         clock.tick(FPS)
 
-main_game()
+while main_loop :
+    if game_state == "menu":
+        menu()
+    if game_state == "game_over":
+        game_over_screen()
+    elif game_state == "playing":
+        game_state = main_game()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT :
+            main_loop = False
+        if event.type == pygame.KEYDOWN:
+            if game_state == "menu":
+                if event.key == pygame.K_1:
+                    game_state = "playing"
+                elif event.key == pygame.K_4:
+                    main_loop = False
+            if game_state == "game_over":
+                if event.key == pygame.K_1:
+                    game_state = "playing"
+                if event.key == pygame.K_2:
+                    game_state == "menu"    
+
+
+    pygame.display.update()
+    clock.tick(FPS)
+pygame.quit()
 
