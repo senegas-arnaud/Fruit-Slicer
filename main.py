f68af42fa3130547c597a76e407ef6d8947b48c3
elodie-boweren <elodie.boweren@laplateforme.io>
2025-01-30 15:44:24 +0100

    feat: add game rules and modified menu

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ YELLOW = (255, 255, 0)
 
 # size screen, background and pictures
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Fruits Ninja") 
+pygame.display.set_caption("Fruit Slicer") 
 background = pygame.image.load("pictures/background.png").convert()
 ninja = pygame.image.load("pictures/ninja.png").convert_alpha()
 
@@ -39,6 +39,7 @@ GRAVITY  = 2
 #text formatting
 title_font = pygame.font.SysFont("Arial", 60, italic = True)
 second_title_font = pygame.font.SysFont("Arial", 30, italic = True)
+game_rule_font = pygame.font.SysFont("Arial", 20, italic = True)
 text_font = pygame.font.SysFont("Arial", 15)
 text_font_bold = pygame.font.SysFont("Arial", 16, bold = True)
 game_letter_font = pygame.font.SysFont("Arial", 35, bold = True)
@@ -109,9 +110,9 @@ def menu():
     screen.blit(background,(0,0))
     screen.blit(ninja,(0,200))
     text("Welcome to", second_title_font, (WHITE), 425, 50)
-    text("NINJA FRUITS", title_font, (WHITE), 300, 100)
-    text("1. PLAY GAME", text_font_bold, (WHITE), 420, 250)
-    text("2. CHANGE YOUR NAME", text_font_bold, (WHITE), 420, 300)
+    text("FRUIT SLICER", title_font, (WHITE), 300, 100)
+    text("1. GAME RULES", text_font_bold, (WHITE), 420, 250)
+    text("2. PLAY GAME", text_font_bold, (WHITE), 420, 300)
     text("3. SCORING TABLE", text_font_bold, (WHITE), 420, 350)
     text("4. EXIT THE GAME (are you sure ?)", text_font_bold, (WHITE), 420, 400)
     text("ENTER YOUR CHOICE", text_font, (WHITE), 460, 450)
@@ -189,6 +190,18 @@ def main_game():
         pygame.display.update()
         clock.tick(FPS)
 
+def game_rules():
+    screen.blit(background,(0,0))
+    text("NINJA SLICER \nGAME RULES", title_font, (WHITE), 300, 60)
+    text("Collect as many points as possible before getting game over.", game_rule_font, (WHITE), 30, 210)
+    text("Type the letter that appears above every fruit entering the screen before \nit disappears.", game_rule_font, (WHITE), 30, 240)
+    text("If you miss the fruit, you lose a life (you start the game with 3 lives)", game_rule_font, (WHITE), 30, 300)
+    text("If you type the letter above an icecube the game will pause for 3 seconds \nthen resume.", game_rule_font, (WHITE), 30, 330)
+    text("You can get extra points by getting a combo of 3 fruits with the same letter.", game_rule_font, (WHITE), 30, 390)
+    text("Be careful though! Make sure you don't type the letter above the bomb, \nor you lose the game immediately!", game_rule_font, (WHITE), 30, 420)
+    text("GOOD LUCK!", second_title_font, (WHITE), 250, 500)
+    text("ESC to return to menu",text_font,(WHITE),200, 560)
+
 objets = [Objet()]
 game_state = "menu"
 main_loop = True
@@ -200,15 +213,21 @@ while main_loop :
         game_state = main_game()
     elif game_state == "game_over":
         game_over_screen()
+    elif game_state == "game_rules":
+        game_state = game_rules()
 
 
     for event in pygame.event.get():
         if event.type == QUIT:
             main_loop = False
         if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                    game_state= "menu"
             if game_state == "menu":
                 if event.key == K_1:
-                    game_state = "playing"
+                    game_state = "game_rules"
+                elif event.key == K_2:
+                    game_state = "playing"  
                 elif event.key == K_4:
                     main_loop = False  
             elif game_state == "game_over":
